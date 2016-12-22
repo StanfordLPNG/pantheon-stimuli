b564632c2370cc277d698e1099919fc16900513a
Francis Y. Yan <francisyyan@stanford.edu>
2016-12-22 10:31:39 -0800

    Remove code irrelavent to pantheon stimuli

--- queue_stimulus/contest_message.hh
+++ queue_stimulus/contest_message.hh
@@ -16,36 +16,36 @@ struct ContestMessage
     uint64_t ack_payload_length;
 
     /* Header for new message */
-    Header( const uint64_t s_sequence_number );
+    Header(const uint64_t s_sequence_number);
 
     /* Parse header from wire */
-    Header( const std::string & str );
+    Header(const std::string & str);
 
     /* Make wire representation of header */
-    std::string to_string( void ) const;
+    std::string to_string(void) const;
   } header;
 
   std::string payload;
 
   /* New message */
-  ContestMessage( const uint64_t s_sequence_number,
-		  const std::string & s_payload );
+  ContestMessage(const uint64_t s_sequence_number,
+                 const std::string & s_payload);
 
   /* Parse incoming datagram from wire */
-  ContestMessage( const std::string & str );
+  ContestMessage(const std::string & str);
 
   /* Fill in the send_timestamp for an outgoing datagram */
-  void set_send_timestamp( void );
+  void set_send_timestamp(void);
 
   /* Make wire representation of datagram */
-  std::string to_string( void ) const;
+  std::string to_string(void) const;
 
   /* Transform into an ack of the ContestMessage */
-  void transform_into_ack( const uint64_t sequence_number,
-			   const uint64_t recv_timestamp );
+  void transform_into_ack(const uint64_t sequence_number,
+                          const uint64_t recv_timestamp);
 
   /* Is this message an ack? */
-  bool is_ack( void ) const;
+  bool is_ack(void) const;
 };
 
 #endif /* CONTEST_MESSAGE_HH */
